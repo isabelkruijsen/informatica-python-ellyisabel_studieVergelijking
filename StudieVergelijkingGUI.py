4471a2a61383cdc09103e6a0d795b75371424876
isabelkruijsen <114383@montessoricollege.nl>
2025-02-27 10:26:06 +0100

    naam anders

---
+++ StudieVergelijkingGUI.py
@@ -0,0 +1,34 @@
+# Dit bestand zorgt voor de gebruikersinterface (GUI)van onze programma.
+# Vul hier de naam van je programma in:
+#Studie vergelijkingen
+#
+# Vul hier jullie namen in:
+#Elly en Isabel
+#
+#
+
+
+### --------- Bibliotheken en globale variabelen -----------------
+from tkinter import *
+import StudieVergelijkingSQL
+
+
+### ---------  Functie definities  -----------------
+
+
+### --------- Hoofdprogramma  ---------------
+
+venster = Tk()
+venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
+venster.wm_title("MC Pizzeria")
+
+
+
+
+
+
+
+
+
+#reageert op gebruikersinvoer, deze regel als laatste laten staan
+venster.mainloop()
---
+++ StudieVergelijkingSQL.py
@@ -0,0 +1,21 @@
+# Vul hier de naam van je programma in:
+# studie vergelijkingen
+#
+# Vul hier jullie namen in:
+#
+#Elly en Isabel
+#
+
+
+### --------- Bibliotheken en globale variabelen -----------------
+
+import sqlite3
+with sqlite3.connect("MCPizzeria.db") as db:
+    cursor = db.cursor()#cursor is object waarmee je data uit de database kan halen
+
+
+### ---------  Functie definities  -----------------
+
+
+### --------- Hoofdprogramma  ---------------
+
