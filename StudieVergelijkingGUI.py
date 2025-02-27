136cfd7d0760d8eec64877abfdb08fa2bd317a0d
isabelkruijsen <114383@montessoricollege.nl>
2025-02-27 10:44:36 +0100

    eerstefdeel

--- StudieVergelijkingGUI.py
+++ StudieVergelijkingGUI.py
@@ -20,7 +20,41 @@ import StudieVergelijkingSQL
 
 venster = Tk()
 venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
-venster.wm_title("MC Pizzeria")
+venster.wm_title("StudieVergelijking")
+
+labelIntro = Label(venster, text="Welkom! Vul hieronder een of meerdere zoektermen in, vervolgens zul je alle opties met jouw ingevulde eisen zien. Als je een studie selecteert, kun je vervolgens meer studie info of reisinformatie opvragen.")
+labelIntro.grid(row=0, column=0, sticky="W")
+
+
+schoolNaam = Label(venster, text="School:")
+schoolNaam.grid(row=1, column=0)
+
+
+ingevoerde_schoolNaam = StringVar ()
+invoerVeldSchoolNaam = Entry(venster, textvariable= ingevoerde_schoolNaam)
+invoerVeldSchoolNaam.grid(row=1, column=1, sticky="W")
+
+
+studieNaam = Label(venster, text="Studie:")
+studieNaam.grid(row=2, column=0)
+
+
+ingevoerde_studieNaam = StringVar ()
+invoerVeldStudieNaam = Entry(venster, textvariable= ingevoerde_studieNaam)
+invoerVeldStudieNaam.grid(row=2, column=1, sticky="W")
+
+
+
+stadNaam = Label(venster, text="Stad:")
+stadNaam.grid(row=3, column=0)
+
+
+ingevoerde_stadNaam = StringVar ()
+invoerVeldStadNaam = Entry(venster, textvariable= ingevoerde_stadNaam)
+invoerVeldStadNaam.grid(row=3, column=1, sticky="W")
+
+KnopZoek = Button(venster, text="Zoek", width= 12, command= zoekStudies)
+KnopZoek.grid(row=1, column=3)
 
 
 
